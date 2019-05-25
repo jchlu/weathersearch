3b81da1b53cb60f84e38fe1da2e868582d1ce4a4
Johnny Choudhury-Lucas <johnny@exito.tech>
2019-05-25 14:32:57 -0400

    Basic form and prevent default onSubmit

---
+++ src/containers/search_bar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { term: '' }
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+  }
+
+  onFormSubmit (event) {
+    event.preventDefault()
+    console.log(this.state.term)
+  }
+
+  render() {
+    return (
+      <form onSubmit={ this.onFormSubmit/* event => {event.preventDefault()} */} className="input-group">
+        <input
+          placeholder='Get a five-day forecast in your favourite cities'
+          className='form-control'
+          value={this.state.term}
+          onChange={event => {this.setState({ term: event.target.value })}}
+        />
+        <span className='input-group-btn'>
+          <button type='submit' className='btn btn-secondary'>Submit</button>
+        </span>
+      </form>
+    )
+  }
+}
