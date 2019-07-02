0a27fe8bcd8b2e2cf6f7fd92b84e3b3b88868bcb
Johnny Choudhury-Lucas <johnny@exito.tech>
2019-07-02 17:53:01 -0400

    Cahange App from HOC to functional component

--- src/components/app.js
+++ src/components/app.js
@@ -3,13 +3,9 @@ import SearchBar from '../containers/search_bar'
 import CountryList from '../containers/country_list'
 import WeatherList from '../containers/weather_list'
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <SearchBar />
-        <WeatherList />
-      </div>
-    )
-  }
-}
+export default () => (
+  <div>
+    <SearchBar />
+    <WeatherList />
+  </div>
+)
